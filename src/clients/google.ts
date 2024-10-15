261be23c1eb69e873714b0cc65c86e0f3c429886
Ali Irani <ali.irani@ovou.com>
2024-10-15 11:34:10 +0330

    Add google integration

---
+++ src/clients/google.ts
@@ -0,0 +1,128 @@
+import { google, Auth } from "googleapis";
+import { createInterface } from "readline";
+
+const readline = createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+async function createOAuth2Client(
+    clientId: string,
+    clientSecret: string,
+    redirectUri: string
+): Promise<Auth.OAuth2Client> {
+    const oauth2Client = new google.auth.OAuth2(
+        clientId,
+        clientSecret,
+        redirectUri
+    );
+
+    const SCOPES = ["https://www.googleapis.com/auth/gmail.send"];
+    const authorizationUrl = oauth2Client.generateAuthUrl({
+        access_type: "offline",
+        scope: SCOPES,
+        prompt: "consent"
+    });
+
+    console.log("Authorize this app by visiting this url:", authorizationUrl);
+
+    const authorizationCode = await new Promise<string>((resolve) => {
+        readline.question("Enter the authorization code: ", (code: string) => {
+            readline.close();
+            resolve(code.trim());
+        });
+    });
+    console.log("Authorization code:", authorizationCode);
+
+    try {
+        const { tokens } = await oauth2Client.getToken(authorizationCode);
+        oauth2Client.setCredentials(tokens);
+
+        console.log("Refresh token:", tokens.refresh_token);
+
+        console.log("OAuth2 client created successfully");
+        return oauth2Client;
+    } catch (error) {
+        console.error("Error creating OAuth2 client:", error);
+        throw error;
+    }
+}
+
+export function createOAuth2ClientFromRefreshToken(
+    clientId: string,
+    clientSecret: string,
+    refreshToken: string
+): Auth.OAuth2Client {
+    const oauth2Client = new google.auth.OAuth2(clientId, clientSecret);
+
+    oauth2Client.setCredentials({
+        refresh_token: refreshToken
+    });
+
+    return oauth2Client;
+}
+
+let oauth2Client: Auth.OAuth2Client | null = null;
+export async function getOAuth2Client(
+    clientId: string,
+    clientSecret: string,
+    refreshToken?: string,
+    redirectUri?: string
+) {
+    if (oauth2Client) {
+        return oauth2Client;
+    }
+
+    if (refreshToken) {
+        oauth2Client = createOAuth2ClientFromRefreshToken(
+            clientId,
+            clientSecret,
+            refreshToken
+        );
+    } else {
+        oauth2Client = await createOAuth2Client(
+            clientId,
+            clientSecret,
+            redirectUri ?? ""
+        );
+    }
+    return oauth2Client;
+}
+
+export type Email = {
+    to: string;
+    subject: string;
+    body: string;
+};
+export async function sendEmail(
+    auth: Auth.OAuth2Client,
+    email: Email
+): Promise<void> {
+    const gmail = google.gmail({ version: "v1", auth });
+
+    const message = [
+        `To: ${email.to}`,
+        `Subject: ${email.subject}`,
+        "",
+        email.body
+    ].join("\n");
+
+    const encodedMessage = Buffer.from(message)
+        .toString("base64")
+        .replace(/\+/g, "-")
+        .replace(/\//g, "_")
+        .replace(/=+$/, "");
+
+    try {
+        await gmail.users.messages.send({
+            userId: "me",
+            requestBody: {
+                raw: encodedMessage
+            }
+        });
+        console.log("Email sent successfully");
+    } catch (error) {
+        console.error("Error sending email:", error);
+        throw error;
+    }
+}
--- src/environments.ts
+++ src/environments.ts
@@ -7,3 +7,11 @@ export const NOTION_INTEGRATION_TOKEN = process.env.NOTION_INTEGRATION_TOKEN;
 export const NOTION_DATABASE_ID = process.env.NOTION_DATABASE_ID ?? "";
 
 export const MAKE_WEBHOOK_URL = process.env.MAKE_WEBHOOK_URL ?? "";
+
+export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID ?? "";
+export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET ?? "";
+export const GOOGLE_REFRESH_TOKEN = process.env.GOOGLE_REFRESH_TOKEN ?? "";
+export const GOOGLE_REDIRECT_URI = process.env.GOOGLE_REDIRECT_URI ?? "";
+
+export const GOOGLE_EMAIL = process.env.GOOGLE_EMAIL ?? "";
+export const GOOGLE_SUBJECT = process.env.GOOGLE_SUBJECT ?? "";
