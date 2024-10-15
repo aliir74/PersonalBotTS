7029ea2a4cffdf5f85af155fc93ca7def94fa86b
Ali Irani <ali.irani@ovou.com>
2024-10-15 11:35:15 +0330

    Add notion to email integration

---
+++ src/integrations/notionToEmail.ts
@@ -0,0 +1,35 @@
+import { getOAuth2Client, Email, sendEmail } from "../clients/google";
+import {
+    GOOGLE_CLIENT_SECRET,
+    GOOGLE_CLIENT_ID,
+    GOOGLE_REFRESH_TOKEN,
+    GOOGLE_REDIRECT_URI,
+    GOOGLE_EMAIL,
+    GOOGLE_SUBJECT
+} from "../environments";
+
+import { getTasksByDueDate, NotionTask } from "../exports/notion";
+
+export async function notionToEmail() {
+    const tasks: NotionTask[] = await getTasksByDueDate(new Date());
+    const emailTasks = tasks.map((task) => {
+        return {
+            to: GOOGLE_EMAIL,
+            subject: GOOGLE_SUBJECT,
+            body: task.properties.taskName
+        } as Email;
+    });
+    const oauth2Client = await getOAuth2Client(
+        GOOGLE_CLIENT_ID,
+        GOOGLE_CLIENT_SECRET,
+        GOOGLE_REFRESH_TOKEN,
+        GOOGLE_REDIRECT_URI
+    );
+    await Promise.all(
+        emailTasks.map(async (task) => {
+            await sendEmail(oauth2Client, task);
+            console.log(`Email sent for task: ${task.body}`);
+        })
+    );
+    console.log("All emails sent");
+}
