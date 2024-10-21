1be74bc34e9ead45a2dc4e7f3b57a42072bacc2d
Ali Irani <ali.irani@ovou.com>
2024-10-21 22:56:44 +0330

    Add deployment bot message

--- src/clients/telegram/bot.ts
+++ src/clients/telegram/bot.ts
@@ -1,5 +1,7 @@
 import { Bot } from "grammy";
-import { TELEGRAM_BOT_TOKEN } from "../../environments";
+import { TELEGRAM_BOT_TOKEN, MY_TELEGRAM_USER_ID } from "../../environments";
 export const bot = new Bot(TELEGRAM_BOT_TOKEN);
 
 bot.start();
+
+bot.api.sendMessage(MY_TELEGRAM_USER_ID, `✔️Code is deployed successfully!`);
