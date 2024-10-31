b7e08f75f4afac8ad4e0f825175b80e6b5427988
Ali Irani <ali.irani@ovou.com>
2024-10-31 21:47:20 +0330

    Refactor

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
-import { notionToEmail } from "./integrations/notionToEmail";
+import { notionToEmail } from "./jobs/notionToEmail";
 import { schedule } from "node-cron";
-import { clickupToNotion } from "./integrations/clickupToNotion";
-import { mrbilitToTelegram } from "./integrations/mrbilitToTelegram";
+import { clickupToNotion } from "./jobs/clickupToNotion";
+import { mrbilitToTelegram } from "./jobs/mrbilitToTelegram";
 import { FilterTrain } from "./clients/mrbilit/types";
 import { MY_TELEGRAM_USER_ID, TELEGRAM_GROUP_ID } from "./environments";
 import { bot } from "./clients/telegram/bot";
-import "./integrations/telegramBot";
+import "./jobs/telegramBot";
 import { log } from "./clients/logger";
+import { automateNotionWorkLog } from "./jobs/automateNotionWorkLog";
 // Every 15 minutes, between 08:00 AM and 11:59 PM, All days
 schedule("*/15 8-23 * * *", async () => {
     try {
@@ -86,4 +87,18 @@ schedule("*/15 * * * *", async () => {
 // Every 15 minutes, Bot active check
 schedule("0 0 * * *", async () => {
     await log("Bot is active", "Bot active check", "success", true);
+});
+
+// Every 15 minutes, Automate Notion Worklog
+schedule("*/15 8-23 * * *", async () => {
+    try {
+        await automateNotionWorkLog();
+    } catch (error) {
+        await log(
+            (error as Error).message,
+            "Automate Notion Worklog",
+            "error",
+            true
+        );
+    }
 });
